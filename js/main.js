78cf9cc0f6860eb7ae4221ab628360ba5454770d
Ren Jeka <renjeka@gmail.com>
2019-09-10 22:25:05 +0300

    Позиционирование и первичная настройка Портфолио

---
+++ js/main.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function () {
+	function $(query) {
+		return document.querySelector(query);
+	}
+
+	var mainContainer = $(".container");
+
+	mainContainer.style.height = window.innerHeight + "px";
+
+
+
+
+	
+});
