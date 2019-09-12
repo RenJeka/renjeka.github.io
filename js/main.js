6614328f9462403873ee737e788eebc36b617ad4
Ren Jeka <renjeka@gmail.com>
2019-09-13 00:49:46 +0300

    Стартовая страница — Версия 2.0

--- js/main.js
+++ js/main.js
@@ -5,12 +5,18 @@ window.addEventListener("load", function () {
 
 	var mainContainer = $(".container");
 
+	var items = document.querySelectorAll(".item");
+
 	mainContainer.style.minHeight = window.innerHeight + "px";
 
 	window.addEventListener("resize", function () {
 		mainContainer.style.minHeight = window.innerHeight + "px";
 	});
 
-
+	for (let i = 0; i< items.length; i++) {
+		items[i].addEventListener("click", function () {
+			window.location.href = this.dataset.href;
+		});
+	}
 	
 });
