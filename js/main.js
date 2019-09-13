da691f1882db796f0f1c1c1deccff7ea4ce1461b
RenJeka <renjeka@gmail.com>
2019-09-13 17:54:41 +0300

    добавлен фон для каждого .item (для каждой карточки сайта)

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ window.addEventListener("load", function () {
 
 	var items = document.querySelectorAll(".item");
 	var socialIcons = document.querySelectorAll(".social-icons img");
+	var itemBgImage;
 
 	mainContainer.style.minHeight = window.innerHeight + "px";
 
@@ -14,7 +15,25 @@ window.addEventListener("load", function () {
 		mainContainer.style.minHeight = window.innerHeight + "px";
 	});
 
+
+	//Цикл, который перебирает блоки с сайтами (блоки с классом .item)
 	for (let i = 0; i< items.length; i++) {
+		
+		// Устанавливаем фон для каждого блока
+		if (items[i].dataset.bg) {
+
+			console.dir(items[i].style);
+			itemBgImage = items[i].dataset.bg.substr(1, items[i].dataset.bg.length -1);
+			items[i].style.backgroundImage = "url("+itemBgImage+")";
+			// items[i].style.backgroundImage = "url("+items[i].dataset.bg+");";
+			
+			console.dir(items[i].style.backgroundImage);
+			
+
+
+		}
+		
+		//	Установлавливаем обработчик событий для каждого блока (переход на нужный сайт)
 		items[i].addEventListener("click", function () {
 			window.location.href = this.dataset.href;
 		});
