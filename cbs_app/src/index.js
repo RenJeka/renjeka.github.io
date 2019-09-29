c411a3758c3e6b66fd79ffa5d98e16b953da5fd6
Ren Jeka <renjeka@gmail.com>
2019-09-29 18:32:03 +0300

    cbs_app→ add new functions : addInfo / whatHead

--- cbs_app/src/index.js
+++ cbs_app/src/index.js
@@ -3,23 +3,20 @@ window.addEventListener("load", () =>{
 		return document.querySelector(selector);
 	}
 
-	let myTable1 = $('.myTable1');
-
-	console.dir(myTable1);
-	console.log(myTable1.rows);
-
+	// Функция, которая добавляет ряды и ячейки в таблицу
 	function addRow(table, numberOfRows, numberOfCells, textInCell) {
 
+		// Цикл добавляеи по 1-й строке то кол-во, которое мы укажем (numberOfRows)
 		for (let i = 0; i < numberOfRows; i++) {
 			let row = document.createElement("tr")
-			
-			console.dir(row);
-			// console.dir(cell);
-			// row.appendChild(cell)
 			table.appendChild(row)
+
+			// Цикл добавляет по 1-й ячейке то кол-во, которое мы укажем (numberOfCells)
 			for (let j = 0; j < numberOfCells; j++) {
 				let cell = document.createElement("td")
 				row.appendChild(cell)
+
+				// Добавляем текст в ячейку переданным параметром
 				cell.innerHTML = textInCell;
 	
 			}
@@ -27,6 +24,52 @@ window.addEventListener("load", () =>{
 		}
 	}
 
+	// Функция заполняет одну указанную ячейку в указанном ряде таблицы. Данные берутся с переданного объекта. Функция переберает объект на подходящее свойство и помещает значение этого свойства в ячейку таблицы.
+	function addInfo(table, object, numberOfRow, numberOfCell  ) {
+
+		for (const key in object) {
+			if (object.hasOwnProperty(key)) {
+				if (key.toLowerCase() === whatHead(table, numberOfCell).toLowerCase()){
+					table.rows[numberOfRow].cells[numberOfCell-1].innerHTML = object[key];
+					console.dir(key);
+				}
+			}
+		}
+	}
+
+	// Функция вычисляет, какой заголовок находиться над указанной ячейкой (Заголовок колонки)
+	function whatHead(table, NumberOfCell) {
+		let nameOfTableHead;
+
+		nameOfTableHead = table.rows[0].cells[NumberOfCell-1].innerHTML;
+
+		return nameOfTableHead;
+
+	}
+
+
+	//============================================================================
+	// Начало программы — вызов функции
+
+	let myTable1 = $('.myTable1');
+	
+	console.dir(myTable1);
+	console.log(myTable1.rows);
+
 	addRow(myTable1,3,3, "any")
 
+	
+	console.log(whatHead(myTable1,3));
+
+	let book1 ={
+		id:332,
+		name: "book1",
+		author: "Jeka",
+		pages: 225,
+		somedata: "hihihi"
+
+	}
+	
+	addInfo(myTable1, book1, 4,3);
+	
 })
