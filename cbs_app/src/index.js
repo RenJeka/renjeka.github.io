fb92b9530b5cb0af11e4902ab030953af5ed806b
Ren Jeka <renjeka@gmail.com>
2019-09-29 19:02:10 +0300

    Create tableWorker object. Moved general functions to that object

--- cbs_app/src/index.js
+++ cbs_app/src/index.js
@@ -1,50 +1,7 @@
+import { tableWorker } from './module1';
 window.addEventListener("load", () =>{
 	function $(selector) {
 		return document.querySelector(selector);
-	}
-
-	// Функция, которая добавляет ряды и ячейки в таблицу
-	function addRow(table, numberOfRows, numberOfCells, textInCell) {
-
-		// Цикл добавляеи по 1-й строке то кол-во, которое мы укажем (numberOfRows)
-		for (let i = 0; i < numberOfRows; i++) {
-			let row = document.createElement("tr")
-			table.appendChild(row)
-
-			// Цикл добавляет по 1-й ячейке то кол-во, которое мы укажем (numberOfCells)
-			for (let j = 0; j < numberOfCells; j++) {
-				let cell = document.createElement("td")
-				row.appendChild(cell)
-
-				// Добавляем текст в ячейку переданным параметром
-				cell.innerHTML = textInCell;
-	
-			}
-			
-		}
-	}
-
-	// Функция заполняет одну указанную ячейку в указанном ряде таблицы. Данные берутся с переданного объекта. Функция переберает объект на подходящее свойство и помещает значение этого свойства в ячейку таблицы.
-	function addInfo(table, object, numberOfRow, numberOfCell  ) {
-
-		for (const key in object) {
-			if (object.hasOwnProperty(key)) {
-				if (key.toLowerCase() === whatHead(table, numberOfCell).toLowerCase()){
-					table.rows[numberOfRow].cells[numberOfCell-1].innerHTML = object[key];
-					console.dir(key);
-				}
-			}
-		}
-	}
-
-	// Функция вычисляет, какой заголовок находиться над указанной ячейкой (Заголовок колонки)
-	function whatHead(table, NumberOfCell) {
-		let nameOfTableHead;
-
-		nameOfTableHead = table.rows[0].cells[NumberOfCell-1].innerHTML;
-
-		return nameOfTableHead;
-
 	}
 
 
@@ -56,10 +13,10 @@ window.addEventListener("load", () =>{
 	console.dir(myTable1);
 	console.log(myTable1.rows);
 
-	addRow(myTable1,3,3, "any")
+	tableWorker.addRow(myTable1,3,3, "any")
 
 	
-	console.log(whatHead(myTable1,3));
+	// console.log(whatHead(myTable1,3));
 
 	let book1 ={
 		id:332,
@@ -70,6 +27,7 @@ window.addEventListener("load", () =>{
 
 	}
 	
-	addInfo(myTable1, book1, 4,3);
+	// addInfoInCell(myTable1, book1, 4,3);
+	tableWorker.addInfoInRow(myTable1,1,book1);
 	
 })
---
+++ cbs_app/src/module1.js
@@ -0,0 +1,50 @@
+
+export let tableWorker = {
+	// Функция, которая добавляет ряды и ячейки в таблицу
+	addRow(table, numberOfRows, numberOfCells, textInCell) {
+
+		// Цикл добавляеи по 1-й строке то кол-во, которое мы укажем (numberOfRows)
+		for (let i = 0; i < numberOfRows; i++) {
+			let row = document.createElement("tr")
+			table.appendChild(row)
+
+			// Цикл добавляет по 1-й ячейке то кол-во, которое мы укажем (numberOfCells)
+			for (let j = 0; j < numberOfCells; j++) {
+				let cell = document.createElement("td")
+				row.appendChild(cell)
+
+				// Добавляем текст в ячейку переданным параметром
+				cell.innerHTML = textInCell;
+			}
+		}
+	},
+
+	// Функция заполняет одну указанную ячейку в указанном ряде таблицы. Данные берутся с переданного объекта. Функция переберает объект на подходящее свойство и помещает значение этого свойства в ячейку таблицы. Нужна для функции addInfoInRow
+	addInfoInCell(table, object, indexOfRow, indexOfCell ) {
+
+		for (const key in object) {
+			if (object.hasOwnProperty(key)) {
+				if (key.toLowerCase() === this.whatHead(table, indexOfCell).toLowerCase()){
+					table.rows[indexOfRow].cells[indexOfCell].innerHTML = object[key];
+					console.dir(key);
+				}
+			}
+		}
+	},
+
+	// Функция вычисляет, какой заголовок находиться над указанной ячейкой (Заголовок колонки). Нужна для функции "addInfoInCell"
+	whatHead(table, indexOfCell) {
+
+		let nameOfTableHead = table.rows[0].cells[indexOfCell].innerHTML;
+		return nameOfTableHead;
+	},
+
+	addInfoInRow(table, indexOfRow, object) {
+
+		for (let k = 0; k < table.rows[indexOfRow].cells.length; k++) {
+			
+			this.addInfoInCell(table, object, indexOfRow, k)
+		}
+	}
+}
+
