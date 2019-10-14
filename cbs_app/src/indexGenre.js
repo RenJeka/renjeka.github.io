30e70d332c3e5d3bf461aae09eab55b366c791cd
Ren Jeka <renjeka@gmail.com>
2019-10-14 18:31:57 +0300

    Создание новой таблицы для жанров. Добавлен новый файл indexGenre.js исправить ошибку импорта в дальнейшем коммите

---
+++ cbs_app/src/indexGenre.js
@@ -0,0 +1,26 @@
+
+import { tableWorker } from './tableWorker.js';
+import { $ } from './myHelperLib.js';
+import { formHandler } from './formHandler.js';
+
+window.addEventListener("load", ()=>{
+	let table = $("#table-genres");
+
+	tableWorker.fillTable(table,"Genres-Library");
+
+	$(".add").addEventListener("click", ()=>{
+
+		// Запускаем обработчик события на клик по кнопке "add" и получаем возращаемый объект, который записался в базу данных.
+		let returnedObject =  formHandler.addBookHandler(e);
+
+		// Если есть ошибка в валидации — возвращаем "false", и клик не дает результата (не записывает данные и не модифицирует таблицу )
+		if (returnedObject == false) {
+			return false;
+		}
+
+		// Добавляем ряд с данными в таблицу
+		tableWorker.addRow(table, returnedObject.addedObject);
+		tableWorker
+
+	})
+})
