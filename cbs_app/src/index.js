ebd10563c02187e9b1076ffc04a2bacda99c27bc
RenJeka <renjeka@gmail.com>
2019-10-21 17:51:00 +0300

    Доработки комментариев

--- cbs_app/src/index.js
+++ cbs_app/src/index.js
@@ -5,29 +5,31 @@ import { formHandler } from './formHandler';
 window.addEventListener("load", () =>{
 	console.dir(document.forms[0])
 	console.dir(document.forms[0].elements)
-	let currentTable = $('#table-books');
-	let localStorageKey = formHandler.getLocalStorageKey();
-	let sortingFlag = false;
+	let currentTable 	= $('#table-books'),
+		localStorageKey = formHandler.getLocalStorageKey(),
+		sortingFlag 	= false;
 
-	// // Находим localStorageKey (Ключ для базы данных в LocalStorage)
-	// for (let i = 0; i < document.forms[0].elements.length; i++) {
-	// 	if (document.forms[0].elements[i].dataset.hasOwnProperty("localStorageKey")) {
-	// 		localStorageKey = document.forms[0].elements[i].dataset.localStorageKey;
-	// 	}
-	// }
+	/*
+		// Находим localStorageKey (Ключ для базы данных в LocalStorage)
+	for (let i = 0; i < document.forms[0].elements.length; i++) {
+		if (document.forms[0].elements[i].dataset.hasOwnProperty("localStorageKey")) {
+			localStorageKey = document.forms[0].elements[i].dataset.localStorageKey;
+		}
+	}
+	*/
+	
+	// Проверяем форму на нужные поля ввода, которые необходимо заполнить по привязке
 	formHandler.checkForm();
-	tableWorker.fillTable(currentTable,localStorageKey);
 
+	// Заполняем текущую таблицу данными из localStorage
+	tableWorker.fillTable(currentTable,localStorageKey);
 
 	// TODO ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
 	// ♦ Задержка строки (через флаг, после нажатия на строку,  строка будет подсвечиваться (эта строка сейчас активна)). На основе этого будет удаление и изменение текущего объекта.
-	// ♦ Подвязать жанры, значениее которых будет передаваться в поля ввода
 	// ♦ реализовать перебор объектов с помощью кнопок (данные выводятся в форму). 
 	// ♦ Добавить возможность изменить объект (перезаписать) (кнопка "Изменить")
 	// ♦ Добавить возможность удалить объект  () (кнопка "Удалить")
-	// ♦ Реализовать поиск . Пока по 1 полю. Фун-я fillTable перезаполняет таблицу с учетом поиска на каждое событие onKeyPress. Посмотреть как это реализовывалось на практической по ангуляру.
 	// ♦ Продумать, как будет реализовываться валидация полей по заданным регулярным выражениям (можно РВ поместить в атрибуты).  
-
 	// TODO ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
 
 	// Обработчик событий по нажатии на кнопку "addBook"
@@ -40,7 +42,6 @@ window.addEventListener("load", () =>{
 		if (returnedObject == false) {
 			return false;
 		}
-
 		// Добавляем ряд с данными в таблицу
 		tableWorker.addRow(currentTable, returnedObject.addedObject);
 	});
@@ -64,13 +65,12 @@ window.addEventListener("load", () =>{
 		});
 	}
 
-
 	// Обработчик поиска
-
 	$('#input-search').addEventListener("keyup", (e)=>{
-		// console.dir(e.target);
-		console.dir(tableWorker.search(e.target, localStorageKey, e.target.dataset.searchObjectProperty));
+		// Очищаем текущую таблицу
 		tableWorker.cleanTable(currentTable);
+
+		// Заполняем таблицу тем массивам, который возвращает фун-я "tableWorker.search"
 		tableWorker.fillTable(currentTable, tableWorker.search(e.target, localStorageKey, e.target.dataset.searchObjectProperty))
 	})
 
