626e2b641911ada64ffe7f6c42e372343d998ae2
RenJeka <renjeka@gmail.com>
2019-10-25 17:53:10 +0300

    Все функции работают с помощью методов класса Table

--- cbs_app/src/index.js
+++ cbs_app/src/index.js
@@ -19,8 +19,8 @@ window.addEventListener("load", () =>{
 	// Создаем объект "tableBook" типа "Table"
 	let tableBook = new Table('#table-books',document.forms[0]);
 
-	// // Добавляем наблюдателей (наблюдателям будет идти рассылка после изменения параметров, изначально параметр "tableData")
-	// tableBook.addObserver(tableWorker);
+	// Добавляем наблюдателей (наблюдателям будет идти рассылка после изменения параметров, изначально параметр "tableData")
+	tableBook.addObserver(tableBook);
 
 	// Проверяем форму на нужные поля ввода, которые необходимо заполнить по привязке
 	formHandler.checkForm();
@@ -31,40 +31,42 @@ window.addEventListener("load", () =>{
 	//Сообщаем наблюдателям об изменении
 	tableBook.notify();
 
-
-	// // Заполняем текущую таблицу данными из localStorage. Возращаем массив с данными.
-	// tableData = tableWorker.fillTable(currentTable,localStorageKey);
-
 	////===========================================================================================
-	// // ОБРАБОТЧИК КНОПКИ "addBook"
-	// $('#addBook').addEventListener("click", function(e) {
+	// ОБРАБОТЧИК КНОПКИ "addBook"
+	$('#addBook').addEventListener("click", function(e) {
 		
-	// 	//получаем возврат функции в виде объекта с 2-мя свойствами (ключ от LocalStorage и новый (созданный) объект )
-	// 	let returnedObject =  formHandler.addBookHandler(e);
-	// 	// Если есть ошибка в валидации — возвращаем "false", и клик не дает результата (не записывает данные и не модифицирует таблицу )
-	// 	if (returnedObject == false) {
-	// 		return false;
-	// 	}
-	// 	// Добавляем ряд с данными в таблицу
-	// 	tableWorker.addRow(tableBook.currentTable, returnedObject.addedObject);
-	// });
+		// получаем возврат функции в виде объекта с 2-мя свойствами (ключ от LocalStorage и новый (созданный) объект)
+		let returnedObject =  formHandler.addBookHandler(e);
+
+		// Если есть ошибка в валидации — возвращаем "false", и клик не дает результата (не записывает данные и не модифицирует таблицу )
+		if (returnedObject == false) {
+			return false;
+		}
+		// Добавляем ряд с данными в таблицу
+		tableBook.addRows(returnedObject.addedObject);
+	});
 
 	//===========================================================================================
-	// // ОБРАБОТЧИК ПОИСКА
-	// $('#input-search').addEventListener("keyup", (e)=>{
-	// 	// Очищаем текущую таблицу
-	// 	tableWorker.cleanTable(tableBook.currentTable);
+	// ОБРАБОТЧИК ПОИСКА
+	$('#input-search').addEventListener("keyup", (e)=>{
 
-	// 	// Заполняем таблицу тем массивам, который возвращает фун-я "tableWorker.search". Возращаем массив с данными.
-	// 	tableBook.tableData = tableWorker.search(e.target, tableBook.localStorageKey, e.target.dataset.searchObjectProperty);
-	// 	tableBook.notify()
-	// 	tableBook.fillTable()
-	// })
+		// Очищаем текущую таблицу
+		tableBook.cleanTable();
+
+		// Заполняем таблицу тем массивам, который возвращает фун-я "tableWorker.search". Возращаем массив с данными.
+		tableBook.tableData = tableBook.search(e.target, e.target.dataset.searchObjectProperty);
+		tableBook.notify()
+		tableBook.fillTable()
+
+	})
+
+
+	// ОБРАБОТЧИК ВЫБОРА СТРОК 
+	tableBook.rowSelectHandler()
 
 	//#region
 	// //===========================================================================================
-	// // ОБРАБОТЧИК ВЫБОРА СТРОК 
-	// tableWorker.rowSelectHandler(tableBook.currentTable, tableBook.localStorageKey,tableData, testFunc)
+
 
 	// // TODO реализовать, чтобы работало (Чтобы if-ы срабатывали после срабатывания функции "tableWorker.rowSelectHandler")
 	// function testFunc(returnValue) {
@@ -91,7 +93,5 @@ window.addEventListener("load", () =>{
 	// 	}
 	// }
 	//#endregion
-
-
 
 });
--- cbs_app/src/table.class.js
+++ cbs_app/src/table.class.js
@@ -29,6 +29,8 @@ export class Table{
 		this.localStorageKey = formHandler.getLocalStorageKey(form);
 		this.tableData = tableData;
 		this.arrayOfObservers = [];
+		this.sortMark;
+		this.sortDirection = false;
 	}
 
 	/**
@@ -40,14 +42,13 @@ export class Table{
 	}
 
 	/**
-	 * Метод вызывает метод подписчика (паттерн Observer)
+	 * Метод вызывает метод подписчика (паттерн Observer). Этот метод создаваля для синхронизации параметра "tableData" и обработчика выбора строки "rowSelectHandler". Здесь объект класа Table ОДНОВРЕМЕННО и ИЗДАТЕЛЬ и ПОДПИСЧИК.
 	 */
 	notify(){
 		this.arrayOfObservers.forEach(element => {
 			element.observerUpdate(this.currentTable, this.localStorageKey, this.tableData)
 		});
 	}
-
 
 // ---------------------------------------------------------------------------
 	/**
@@ -57,6 +58,13 @@ export class Table{
 	 */
 	fillTable(sortMark,sortDirection) {
 	
+		if (sortMark) {
+			this.sortMark = sortMark;
+		}
+		if (sortDirection) {
+			this.sortDirection = sortDirection;
+		}
+		
 		// Если у нас есть связанная таблица и готовый массив с данными для заполнения таблицы
 		if (this.currentTable && this.tableData) {
 			
@@ -72,7 +80,7 @@ export class Table{
 		}else if (this.currentTable && this.localStorageKey) {
 
 			// Достаем данные из localStorage
-			this.tableData = this.getTableData(sortMark,sortDirection); // ! sort --> [sortMark,sortDirection]
+			this.tableData = this.getTableData();
 
 			// Проверка на наличие данных в localStorage //TODO Проверить, правильная ли проверка
 			if (this.tableData == false) {
@@ -92,7 +100,7 @@ export class Table{
 		}else{
 
 			// Если таблица не подвязана -- выброс исключения
-			if (currentTable == false) {
+			if (this.currentTable == false) {
 				throw `Не найдена связанная HTML-таблица. Пожалуйста добавьте HTML-таблицу в свойство ${this}.currentTable `;
 
 			// Если у нас нет ни массива с данными, ни ключа localStorage -- выброс исключения
@@ -109,25 +117,13 @@ export class Table{
 	 * @param {Boolean} sortDirection Флаг для направления сортировки (прямая или обратная сортировка)
 	 * @return {Object} Возвращает распарсенный массив объектов, которым можно заполнить таблицу (jsonObject)
 	 */
-	getTableData(sortMark,sortDirection) { 
+	getTableData() { 
 
 		let jsonObject = window.localStorage.getItem(this.localStorageKey);
 		let flag;
 
-		
-
 		// Определяем функцию для сравнения (как будет сортироваться массив)
-		function compare(a, b) { 
-			if (sortDirection) {
-				if (a[sortMark] > b[sortMark]) return 1; // если первое значение больше второго
-				if (a[sortMark] == b[sortMark]) return 0; // если равны
-				if (a[sortMark] < b[sortMark]) return -1; // если первое значение меньше второго
-			}else{
-				if (a[sortMark] < b[sortMark]) return 1; 
-				if (a[sortMark] == b[sortMark]) return 0;
-				if (a[sortMark] > b[sortMark]) return -1;
-			}	
-		}
+
 
 		// Если есть JSON-данные по переданному ключу — тогда возвращаем подготовленные данные, если JSON не найден — возвращаем false.
 		if (jsonObject) {
@@ -135,17 +131,17 @@ export class Table{
 			// Парсим данные из localStorage. Получаем массив с объектами
 			jsonObject = JSON.parse(jsonObject);
 
-			// Если поле для сортировки "sortMark" соответствует свойству в объекте
-			if (jsonObject.some((elem)=>elem[sortMark])) {
+			// Если поле для сортировки "this.sortMark" соответствует свойству в объекте
+			if (jsonObject.some((elem)=>elem[this.sortMark])) {
 
 				//Возвращаем отсортированный массив по заранее заданной функции
-				return jsonObject.sort(compare) // !>>>>>>>>
+				return jsonObject.sort(this.compare1) 
 
-			// Если мы не передали поле для сортировки "sortMark" — Возвращаем неотсортированный массив.
-			}else if(sortMark == undefined){
+			// Если нет значения для сортировки "this.sortMark" — Возвращаем неотсортированный массив.
+			}else if(this.sortMark == undefined){
 				return jsonObject;
 
-			// Если поле для сортировки "sortMark" не верное — выбрасываем исключение. 
+			// Если значения для сортировки "this.sortMark" не верное — выбрасываем исключение. 
 			}else{
 				throw "Значение, переданное для сортировки — не верное. Такого свойства у объекта нет."
 			}
@@ -159,7 +155,6 @@ export class Table{
 	/**
 	 * Метод, который добавляет ряды с ячейками в таблицу
 	 * @todo Реализовать эту функцию так, чтобы не получалось замыкание. (Чтобы не использовалась внутреняя функция)
-	 * @param {Object} table Таблица, в которую необходимо добавить ряд(ы)
 	 * @param {object} dataInRow Данные для 1 строки (1 объект)	 
 	 * @param {Number} numberOfRows Кол-во рядов (по умолчанию = 1)
 	 * @param {Number} numberOfCells Кол-во ячеек в ряде (По умолчанию высчитывается по кол-ву ячеек в заголовке (thead))
@@ -209,7 +204,6 @@ export class Table{
 		}
 		return add(this);
 	}
-
 
 // ---------------------------------------------------------------------------
 	/**
@@ -262,6 +256,134 @@ export class Table{
 		// Получаем значение из атрибута "data-object-key-bind" в заголовочной ячейки
 		let nameOfTableHead = this.currentTable.rows[0].cells[indexOfCell].dataset.objectKeyBind;
 		return nameOfTableHead;
+	}
+
+// -----------------------------------------------------------------------------
+	/**
+	 * Метод полностью очищает таблицу 
+	 * @param {number} indexOfRow Индекс строки, которую нужно удалить
+	 */
+	cleanTable(indexOfRow){
+
+		indexOfRow = parseInt(indexOfRow);
+
+		if (indexOfRow) {
+			// Проверка, правильно ли введен индекс строки
+			if (indexOfRow <= this.currentTable.rows.length -1  && indexOfRow >= 0) {
+
+				// Удаляем строку по индексу "indexOfRow"
+				this.currentTable.deleteRow(indexOfRow)
+			}else{
+				throw "Невозможно удалить строку. Такой строки не существует. Проверьте правильность передаваемого в параметр значения."
+			}
+		}else{
+			// Запускаем цикл на иттераций равную кол-во строк в таблице. Каждый раз удаляем последнюю строку.
+			while (this.currentTable.rows.length > 1) {
+				this.currentTable.deleteRow(this.currentTable.rows.length - 1);
+			}
+		}
+	}
+
+// -----------------------------------------------------------------------------
+	/**
+	 * Метод поиска. Фильтрует массив по значению input.value, и возвращает его.
+	 * @param {object} input Элемент, в который вводится фильтрирующее значение.
+	 * @param {string} objectProperty свойство (название свойства) объекта, по которому будет фильтрация.
+	 * @return {Array} Возвращает отфильтнованный массив.
+	 */
+	search(input, objectProperty){
+		console.log("search запущен");
+		console.log(input.value);
+		this.tableData = this.getTableData();
+
+		if(this.tableData){
+
+			return this.tableData.filter(element => {
+				// Переводим значение свойства объекта в нижний регистр 
+				let tempProperty = element[objectProperty].trim().toLocaleLowerCase();
+				// Если "tempProperty" содержит то, что мы ввели в "input" -- значит это искомое значение.
+				return tempProperty.includes(input.value.trim().toLocaleLowerCase());
+			});
+			  
+		}else{
+			console.log ("Не удалось найти массив с данными")
+			return false;
+		}
+		
+	}
+
+// -----------------------------------------------------------------------------
+	/**
+	 * Метод обрабатывает клик по строке. Его задача отсортировать таблицу, если клик был по заголовку, и вернуть строку, если клик был по любой другой строке
+	 * @param {String} localStorageKey Ключ от базы данных (localStorage), с которой берутся данные для заполнения таблицы
+	 * @return {object} Возвращщает массив с данными после сортировки, либо объект, на который кликнул пользователь.
+	 */
+	rowSelectHandler(callback){
+		let tempFunc = callback || function () {},
+		returnValue; // Значение, которое возвращает этот метод.
+		console.log("Запуск rowSelectHandler");
+		
+			
+		//#region
+		// TODO Проблемма: что вернуть? Изначально идея была -- чтобы вернуть объект которым была заполнена строка (объект с массива объектов в Базе данных). Но, если следовать первому условию (мы кликнули на заголовочную ячейку) -- то таблица отсортировалась, и у нас новый массив с данными. Его нужно вернуть туда, откуда этот метод вызывался. А потом при повторном вызове этого метода, но уже когда произошел клик по строке с данными, принять этот массив данных (tableData) и вернуть конкретный объект. Т.е. по идее тут 2 разных return: Один с массивом объектов (при клике по заголовку), и другой -- просто с 1 объектом (при клике на строке с данными)
+		//#endregion
+		
+		// Основной метод -- обработчик событий на клик
+		this.currentTable.addEventListener("click", (e)=>{
+
+			this.sortMark = e.target.dataset.objectKeyBind;
+			
+			// Проверка, если есть атрибут "objectKeyBind" у HTML элемента -- сортируем таблицу
+			if (this.sortMark && e.target.tagName == "TH") {
+
+				this.sortDirection = !this.sortDirection;
+				
+				// Сортируем текущие данные. (Учитывая потерю контекста)
+				this.tableData.sort(this.compare1.bind(this));
+
+				// Очищаем всю таблицу
+				this.cleanTable();
+
+				// Заполняе новыми значениями (с учетом сортировки), и возвращаем массив данных.
+				returnValue = this.fillTable();
+				this.tableData = returnValue;
+				
+			// Если пользователь кликнул по строке с данными -- находим индекс строки и возвращаем объект с массива объектов (по этому индексу)
+			}else if(e.target.tagName == "TD"){
+
+				returnValue = this.tableData[e.target.parentElement.rowIndex -1];
+			}
+
+			tempFunc(returnValue);
+			console.dir(returnValue);
+		})	
+	}
+
+// -----------------------------------------------------------------------------
+	/**
+	 * Метод update паттерна Observer
+	 */
+	observerUpdate(){
+		// Список запускающих методов
+		this.rowSelectHandler();
+	}
+
+	/**
+	 * Метод, который передается в сортировку массива.
+	 * @param {*} a 
+	 * @param {*} b 
+	 */
+	compare1(a, b) { 
+
+		if (this.sortDirection) {
+			if (a[this.sortMark] >  b[this.sortMark]) return 1; // если первое значение больше второго
+			if (a[this.sortMark] == b[this.sortMark]) return 0; // если равны
+			if (a[this.sortMark] <  b[this.sortMark]) return -1; // если первое значение меньше второго
+		}else{
+			if (a[this.sortMark] <  b[this.sortMark]) return 1; 
+			if (a[this.sortMark] == b[this.sortMark]) return 0;
+			if (a[this.sortMark] >  b[this.sortMark]) return -1;
+		}	
 	}
 
 
