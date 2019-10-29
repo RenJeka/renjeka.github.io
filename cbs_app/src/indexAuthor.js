95c069001e282223bc70e23d391c6c25a2614974
RenJeka <renjeka@gmail.com>
2019-10-29 16:46:27 +0200

    Привязка классов Table и  Form для indexAuthor.js и indexGenre.js

--- cbs_app/src/indexAuthor.js
+++ cbs_app/src/indexAuthor.js
@@ -3,48 +3,104 @@ import { tableWorker } from './tableWorker.js';
 import { $ } from './myHelperLib.js';
 import { formHandler } from './formHandler.js';
 
+import { Table } from './table.class';
+import { Form } from './form.class';
+
 
 
 window.addEventListener("load", ()=>{
-	console.dir(document.forms[0])
-	console.dir(document.forms[0].elements)
-	let currentTable = $('#table-authors');
-	let localStorageKey = formHandler.getLocalStorageKey();
-	let sortingFlag = false;
 
-	tableWorker.fillTable(currentTable,localStorageKey);
 
-	$('#add').addEventListener("click", function(e) {
+	let formAuthor = new Form(document.forms[0], "id");
+	let tableAuthor = new Table('#table-authors',formAuthor);
+
+	formAuthor.checkForm();
+
+	tableAuthor.addObservers(tableAuthor, formAuthor);
+
+	tableAuthor.fillTable();
+
+	// ОБРАБОТЧИК КНОПКИ "addBook"
+	$('#add').addEventListener("click", function() {
+
+		let returnedObject = formAuthor.addBookHandler();
+
+	   if (returnedObject == false) {
+		   return false;
+	   }
+	   tableAuthor.addRows(returnedObject);
+	   tableAuthor.notify();
+	   
+   });
+
+   	// // ОБРАБОТЧИК ПОИСКА
+	// $('#input-search').addEventListener("keyup", (e)=>{
+
+	// 	// Очищаем текущую таблицу
+	// 	tableAuthor.cleanTable();
+
+	// 	// Заполняем таблицу тем массивам, который возвращает фун-я "tableWorker.search". Возращаем массив с данными.
+	// 	tableAuthor.tableData = tableAuthor.search(e.target, e.target.dataset.searchObjectProperty);
+	// 	tableAuthor.notify()
+	// 	tableAuthor.fillTable()
+
+	// })
+
+	// ОБРАБОТЧИК ВЫБОРА СТРОК 
+	tableAuthor.tableClickHandler()
+
+
+
+
+
+
+
+
+
+	// console.dir(document.forms[0])
+	// console.dir(document.forms[0].elements)
+	// let currentTable = $('#table-authors');
+	// let localStorageKey = formHandler.getLocalStorageKey();
+	// let sortingFlag = false;
+
+	// tableWorker.fillTable(currentTable,localStorageKey);
+
+	// $('#add').addEventListener("click", function(e) {
 		
-		// Запускаем обработчик события на клик по кнопке "addBook" и получаем возращаемый объект, который записался в базу данных.
-		let returnedObject =  formHandler.addBookHandler(e);
+	// 	// Запускаем обработчик события на клик по кнопке "addBook" и получаем возращаемый объект, который записался в базу данных.
+	// 	let returnedObject =  formHandler.addBookHandler(e);
 
-		// Если есть ошибка в валидации — возвращаем "false", и клик не дает результата (не записывает данные и не модифицирует таблицу )
-		if (returnedObject == false) {
-			return false;
-		}
+	// 	// Если есть ошибка в валидации — возвращаем "false", и клик не дает результата (не записывает данные и не модифицирует таблицу )
+	// 	if (returnedObject == false) {
+	// 		return false;
+	// 	}
 
-		// Добавляем ряд с данными в таблицу
-		tableWorker.addRow(currentTable, returnedObject.addedObject);
-	});
+	// 	// Добавляем ряд с данными в таблицу
+	// 	tableWorker.addRow(currentTable, returnedObject.addedObject);
+	// });
 
-	// Выбор определенной строки. Тут перебираются все строки и на конкретную строку, которую выбрал пользователь -- вешается обработчик событий. 
-	// TODO Реализовать так, чтобы вся логика находилась в файле "tableWorker.js"
-	for (let i = 0; i < currentTable.rows.length; i++) {
-		currentTable.rows[i].addEventListener('click', function (e) {
+	// // Выбор определенной строки. Тут перебираются все строки и на конкретную строку, которую выбрал пользователь -- вешается обработчик событий. 
+	// // TODO Реализовать так, чтобы вся логика находилась в файле "tableWorker.js"
+	// for (let i = 0; i < currentTable.rows.length; i++) {
+	// 	currentTable.rows[i].addEventListener('click', function (e) {
 
-			let sortMark = e.target.dataset.objectKeyBind;
+	// 		let sortMark = e.target.dataset.objectKeyBind;
 
-			if (this.rowIndex == 0) {
+	// 		if (this.rowIndex == 0) {
 
-				sortingFlag = !sortingFlag;
+	// 			sortingFlag = !sortingFlag;
 
- 				tableWorker.cleanTable(currentTable);
-				tableWorker.fillTable(currentTable,localStorageKey, sortMark, sortingFlag);
-			}
-			console.log(`Индекс строки =  ${this.rowIndex}`);
-		});
-	}
+ 	// 			tableWorker.cleanTable(currentTable);
+	// 			tableWorker.fillTable(currentTable,localStorageKey, sortMark, sortingFlag);
+	// 		}
+	// 		console.log(`Индекс строки =  ${this.rowIndex}`);
+	// 	});
+	// }
+
+
+
+
+
 
 
 })
--- cbs_app/src/indexGenre.js
+++ cbs_app/src/indexGenre.js
@@ -2,43 +2,87 @@
 import { tableWorker } from './tableWorker.js';
 import { $ } from './myHelperLib.js';
 import { formHandler } from './formHandler.js';
+import { Table } from './table.class';
+import { Form } from './form.class';
+
 
 window.addEventListener("load", ()=>{
-	let currentTable = $("#table-genres");
-	let localStorageKey = formHandler.getLocalStorageKey();
-	let sortingFlag = false;
 
-	tableWorker.fillTable(currentTable,localStorageKey);
 
-	$("#add").addEventListener("click", (e)=>{
+	let formGenre = new Form(document.forms[0], "id");
+	let tableGenre = new Table('#table-genres',formGenre);
 
-		// Запускаем обработчик события на клик по кнопке "add" и получаем возращаемый объект, который записался в базу данных.
-		let returnedObject =  formHandler.addBookHandler(e);
+	formGenre.checkForm();
 
-		// Если есть ошибка в валидации — возвращаем "false", и клик не дает результата (не записывает данные и не модифицирует таблицу )
-		if (returnedObject == false) {
-			return false;
-		}
+	tableGenre.addObservers(tableGenre, formGenre);
 
-		// Добавляем ряд с данными в таблицу
-		tableWorker.addRow(currentTable, returnedObject.addedObject);
-		// tableWorker
+	tableGenre.fillTable();
 
-	})
+	// ОБРАБОТЧИК КНОПКИ "addBook"
+	$('#add').addEventListener("click", function() {
 
-	for (let i = 0; i < currentTable.rows.length; i++) {
-		currentTable.rows[i].addEventListener('click', function (e) {
+		let returnedObject = formGenre.addBookHandler();
 
-			let sortMark = e.target.dataset.objectKeyBind;
+	   if (returnedObject == false) {
+		   return false;
+	   }
+	   tableGenre.addRows(returnedObject);
+	   tableGenre.notify();
+	   
+   });
 
-			if (this.rowIndex == 0) {
+   	// // ОБРАБОТЧИК ПОИСКА
+	// $('#input-search').addEventListener("keyup", (e)=>{
 
-				sortingFlag = !sortingFlag;
+	// 	// Очищаем текущую таблицу
+	// 	tableGenre.cleanTable();
 
- 				tableWorker.cleanTable(currentTable);
-				tableWorker.fillTable(currentTable,localStorageKey, sortMark, sortingFlag);
-			}
-			console.log(`Индекс строки =  ${this.rowIndex}`);
-		});
-	}
+	// 	// Заполняем таблицу тем массивам, который возвращает фун-я "tableWorker.search". Возращаем массив с данными.
+	// 	tableGenre.tableData = tableGenre.search(e.target, e.target.dataset.searchObjectProperty);
+	// 	tableGenre.notify()
+	// 	tableGenre.fillTable()
+
+	// })
+
+	// ОБРАБОТЧИК ВЫБОРА СТРОК 
+	tableGenre.tableClickHandler()
+
+
+	// let currentTable = $("#table-genres");
+	// let localStorageKey = formHandler.getLocalStorageKey();
+	// let sortingFlag = false;
+
+	// tableWorker.fillTable(currentTable,localStorageKey);
+
+	// $("#add").addEventListener("click", (e)=>{
+
+	// 	// Запускаем обработчик события на клик по кнопке "add" и получаем возращаемый объект, который записался в базу данных.
+	// 	let returnedObject =  formHandler.addBookHandler(e);
+
+	// 	// Если есть ошибка в валидации — возвращаем "false", и клик не дает результата (не записывает данные и не модифицирует таблицу )
+	// 	if (returnedObject == false) {
+	// 		return false;
+	// 	}
+
+	// 	// Добавляем ряд с данными в таблицу
+	// 	tableWorker.addRow(currentTable, returnedObject.addedObject);
+	// 	// tableWorker
+
+	// })
+
+	// for (let i = 0; i < currentTable.rows.length; i++) {
+	// 	currentTable.rows[i].addEventListener('click', function (e) {
+
+	// 		let sortMark = e.target.dataset.objectKeyBind;
+
+	// 		if (this.rowIndex == 0) {
+
+	// 			sortingFlag = !sortingFlag;
+
+ 	// 			tableWorker.cleanTable(currentTable);
+	// 			tableWorker.fillTable(currentTable,localStorageKey, sortMark, sortingFlag);
+	// 		}
+	// 		console.log(`Индекс строки =  ${this.rowIndex}`);
+	// 	});
+	// }
 })
