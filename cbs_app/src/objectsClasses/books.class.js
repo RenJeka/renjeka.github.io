14b3d19b2e4b814f2b8502939a37730b57ed0e37
RenJeka <renjeka@gmail.com>
2019-10-31 16:34:43 +0200

    Изменена структура файлов. Модифицирован README.md

---
+++ cbs_app/src/objectsClasses/books.class.js
@@ -0,0 +1,27 @@
+/**
+ * Класс для создания объектов "Книги". Каждая книга — объект, свойства которого пользователь заполняет в полях "input" в форме "Добавить книгу"
+ */
+class Book{
+	// Конструктор не применялся. Новый объект "book" заполняется с помощью цикла
+/* 	constructor(id, name, author){
+		this.id = id;
+		this.name = name;
+		this.author = author;
+
+	} */
+
+	// constructor(array){
+
+	// 	for (let i = 0; i < array.length; i++) {
+			
+	// 	}
+	// 	// array.forEach(element => {
+	// 	// 	element.id = id;
+	// 	// 	element.name = name;
+	// 	// 	element.author = author;
+	// 	// });
+	// }
+
+}
+
+export {Book};
