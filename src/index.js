f15d79d2cf5e0b855e7d452c2a9ce38618c6b625
RenJeka <renjeka@gmail.com>
2023-02-06 15:20:07 +0200

    added info modal window

--- src/index.js
+++ src/index.js
@@ -1,50 +1,48 @@
 window.addEventListener("load", function () {
 
-	var mainContainer = document.querySelector(".container"), // Главный контейнер
-		items = document.querySelectorAll(".item"),	// Элемент, который указывакт на определенный сайт
-		socialIcons = document.querySelectorAll(".social-icons img"), // Массив с иконками соц-сетей
+	var mainContainer = document.querySelector(".container"),
+		items = document.querySelectorAll(".item"),
+		socialIcons = document.querySelectorAll(".social-icons img"),
 		itemBgImage; 					
 
-	// ======================== НАСТРОЙКА ГЛАВНОГО БЛОКА ==============================
-	// Подгоняем правильный размер для главного блока сайта, чтобы корректно работала адаптация
+	const 	infoIcon = document.querySelector("#infoIcon"),
+			modalOverlay = document.querySelector(".modal-overlay"),
+			modal = document.querySelector(".modal"),
+			modalClose = document.querySelector(".modal-close");
+
+	infoIcon.addEventListener("click", ()=>{
+		modalOverlay.classList.add("modal-overlay-visible");
+		modal.classList.add("modal-visible");
+	});
+
+	modalOverlay.addEventListener("click", (e)=>{
+		modalOverlay.classList.remove("modal-overlay-visible");
+		modal.classList.remove("modal-visible");
+	});
+
+	modalClose.addEventListener("click", ()=>{
+		console.log('modal-close');
+		modalOverlay.classList.remove("modal-overlay-visible");
+		modal.classList.remove("modal-visible");
+	});
+	// ======================== MAIN UNIT SETUP START ======================
+	// We adjust the correct size for the main block of the site so that the adaptation works correctly
 	mainContainer.style.minHeight = window.innerHeight + "px";
 
 	window.addEventListener("resize", function () {
 		mainContainer.style.minHeight = window.innerHeight + "px";
 	});
-	// ======================== КОНЕЦ НАСТРОЙКИ ГЛАВНОГО БЛОКА ========================
+	// ======================== MAIN UNIT SETUP END ========================
 
 
-	/* 
-	// =========================== ЭЛЕМЕНТ ITEM  ======================================
-	//Цикл, который перебирает блоки с сайтами (блоки с классом .item)
-	for (let i = 0; i < items.length; i++) {
-		
-		// Устанавливаем фон для каждого блока
-		if (items[i].dataset.bg) {
-			
-			itemBgImage = items[i].dataset.bg.substr(1, items[i].dataset.bg.length -1); // Парсим строку, чтобы избавиться от кавычек, чтобы потом поместить его как фон для 
-			items[i].style.backgroundImage = "url("+itemBgImage+")";  // Помеще
-			// items[i].style.backgroundImage = "url("+items[i].dataset.bg+");";
-		}
-		
-		//	Установлавливаем обработчик событий для каждого блока (переход на нужный сайт)
-		items[i].addEventListener("click", function () {
-			window.location.href = this.dataset.href;
-		});
-	}
-	// =========================== КОНЕЦ ЭЛЕМЕНТ ITEM  ================================ 
-	*/
-
-
-	// ======================== ИКОНКИ СОЦ-СЕТЕЙ ======================================
-	// Обработчик событий на нажатие по иконкам соцсетей
+	// ======================== SOCIAL ICONS START  ======================================
+	// Click event handler for social media icons
 	for (let j = 0; j < socialIcons.length; j++) {
 
 		socialIcons[j].addEventListener("click", function () {
 			window.open (this.dataset.href);
 		});
 	}
-	// ======================== КОНЕЦ ИКОНКИ СОЦ-СЕТЕЙ =================================
+	// ======================== SOCIAL ICONS END  =================================
 	
 });
