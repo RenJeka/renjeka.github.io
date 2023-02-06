8e2cb59d634077003e46731c8ef861d6801a940c
RenJeka <renjeka@gmail.com>
2023-02-06 10:08:34 +0200

    translated comments to english in index.html & main.js

--- js/main.js
+++ js/main.js
@@ -1,53 +1,53 @@
 window.addEventListener("load", function () {
 
-	// Вспомогательная функция для захвата элемента
+	// Helper function to capture the element
 	function $(query) {
 		return document.querySelector(query);
 	}
 
-	var mainContainer = $(".container"), // Главный контейнер
-		items = document.querySelectorAll(".item"),	// Элемент, который указывакт на определенный сайт
-		socialIcons = document.querySelectorAll(".social-icons img"), // Массив с иконками соц-сетей
-		itemBgImage; 					// 
+	var mainContainer = $(".container"),
+		items = document.querySelectorAll(".item"),	// An element that points to a specific site
+		socialIcons = document.querySelectorAll(".social-icons img"),
+		itemBgImage; 
 
-	// ======================== НАСТРОЙКА ГЛАВНОГО БЛОКА ==============================
-	// Подгоняем правильный размер для главного блока сайта, чтобы корректно работала адаптация
+	// ======================== MAIN UNIT SETUP START ==============================
+	// We adjust the correct size for the main block of the site so that the adaptation works correctly
 	mainContainer.style.minHeight = window.innerHeight + "px";
 
 	window.addEventListener("resize", function () {
 		mainContainer.style.minHeight = window.innerHeight + "px";
 	});
-	// ======================== КОНЕЦ НАСТРОЙКИ ГЛАВНОГО БЛОКА ========================
+	// ======================== MAIN UNIT SETUP END ========================
 
 
-	// =========================== ЭЛЕМЕНТ ITEM  ======================================
-	//Цикл, который перебирает блоки с сайтами (блоки с классом .item)
+	// =========================== ITEM ELEMENT START ======================================
+	// A loop that loops through blocks with websites (blocks with the .item class)
 	for (let i = 0; i < items.length; i++) {
 		
-		// Устанавливаем фон для каждого блока
+		// Set the background for each block
 		if (items[i].dataset.bg) {
 			
-			itemBgImage = items[i].dataset.bg.substr(1, items[i].dataset.bg.length -1); // Парсим строку, чтобы избавиться от кавычек, чтобы потом поместить его как фон для 
+			itemBgImage = items[i].dataset.bg.substr(1, items[i].dataset.bg.length -1); // We parse the background path to get rid of the quotes so that we can later place it as a background for the card
 			items[i].style.backgroundImage = "url("+itemBgImage+")";  // Помеще
 			// items[i].style.backgroundImage = "url("+items[i].dataset.bg+");";
 		}
 		
-		//	Установлавливаем обработчик событий для каждого блока (переход на нужный сайт)
+		//	Set an event handler for each block (go to the desired site)
 		items[i].addEventListener("click", function () {
 			window.location.href = this.dataset.href;
 		});
 	}
-	// =========================== КОНЕЦ ЭЛЕМЕНТ ITEM  ================================
+	// =========================== ITEM ELEMENT END  ================================
 	
 
-	// ======================== ИКОНКИ СОЦ-СЕТЕЙ ======================================
-	// Обработчик событий на нажатие по иконкам соцсетей
+	// ======================== SOCIAL ICONS START ======================================
+	// Click event handler for social media icons
 	for (let j = 0; j < socialIcons.length; j++) {
 
 		socialIcons[j].addEventListener("click", function () {
 			window.open (this.dataset.href);
 		});
 	}
-	// ======================== КОНЕЦ ИКОНКИ СОЦ-СЕТЕЙ =================================
+	// ======================== SOCIAL ICONS END =================================
 	
 });
